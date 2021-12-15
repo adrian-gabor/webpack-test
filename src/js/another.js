1c646c53b2fd9090e55fb9d3070093ddb8fea58a
Adrian Gabor <adgabor97@gmail.com>
2021-12-15 18:54:20 +0100

    localStorage2 css

--- src/js/another.js
+++ src/js/another.js
@@ -1,16 +1,19 @@
 import '../scss/main.scss';
+import '../scss/another.scss';
 console.log('hello another');
 
-const textarea = document.querySelector('.form_item--js')
+const textarea = document.querySelector('.textarea--js')
 const load = document.querySelector('.load--js')
 const save = document.querySelector('.save--js')
 
 
 
-load.addEventListener('click', () => {
+load.addEventListener('click', (e) => {
+    e.preventDefault();
     textarea.value = localStorage.getItem('entry')
 })
 
-save.addEventListener('click', () => {
+save.addEventListener('click', (e) => {
+    e.preventDefault();
     localStorage.setItem('entry', textarea.value)
 })
